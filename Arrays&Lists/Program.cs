85bc0145d2cb316aea06cc6ca1a34eff6714e2d2
SarveshKadam-NimapInfotech <Sarveshkadam@nimapinfotech.com>
2023-10-27 19:30:16 +0530

    Arrays&Lists, DateTime and TimeSpan directories Added

---
+++ Arrays&Lists/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Arrays_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------------------------Arrays-------------------------
+            //int[] numbers = new int[] { 3, 7, 9, 2, 14, 6 };
+
+            ////Length
+            //Console.WriteLine("length:" + numbers.Length);
+
+            ////IndexOf()
+            //var index = Array.IndexOf(numbers, 9);
+            //Console.WriteLine("Index of 9:" + index);
+
+            ////Clear()
+            //Array.Clear(numbers, 0, 2);
+
+            //Console.WriteLine("Effects of Clear()");
+            //foreach (int i in numbers) Console.WriteLine(i);
+
+            ////Copy()
+            //int[] another = new int[3];
+            //Array.Copy(numbers, another, 3);
+
+            //Console.WriteLine("Effects of Copy()");
+            //foreach (int i in another) Console.WriteLine(i);
+
+            ////Reverse()
+            //Array.Reverse(numbers);
+            //Console.WriteLine("Effects of Reverse()");
+            //foreach (int i in numbers) Console.WriteLine(i);
+
+            //-------------------------------Lists-----------------------------------
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[] { 5, 6, 7 });
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Index os 1 :" + numbers.IndexOf(1));
+            Console.WriteLine("Last Index os 1 : " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count :" + numbers.Count);
+
+            for(int i = 0; i <numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+                
+            }
+             foreach(var i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            numbers.Clear();
+            Console.WriteLine("Count : " + numbers.Count);
+
+
+
+
+
+        }
+    }
+}
---
+++ DateTime/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DateTimeAndTimeSpan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------DateTime----------------------
+            //var dateTime = new DateTime(2015, 1, 1);
+            //var now = DateTime.Now;
+            //var today = DateTime.Today;
+
+            //Console.WriteLine("Hours: " + now.Hour);
+            //Console.WriteLine("Minute: " + now.Minute);
+
+            //var tomorrow = now.AddDays(1);
+            //var yesterday = today.AddDays(-1);
+            //Console.WriteLine(tomorrow);
+            //Console.WriteLine(yesterday);
+
+            //Console.WriteLine(now.ToLongDateString());
+            //Console.WriteLine(now.ToShortDateString());
+            //Console.WriteLine(now.ToLongTimeString());
+            //Console.WriteLine(now.ToShortTimeString());
+            //Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
+            //---------------------------TimeSpan-----------------------------
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration :" + duration);
+
+            //Properties
+            Console.WriteLine("Minutes: "+ timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            //Add 
+            Console.WriteLine(timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine(timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            //ToString
+            Console.WriteLine(timeSpan.ToString());
+
+            //Parse
+            Console.WriteLine(TimeSpan.Parse("01:02:03"));
+
+
+
+
+
+
+
+
+        }
+    }
+}
